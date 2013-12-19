ea025cec5e0e0e70a166f938bbd53f4e30576841
Luciano Rossi <lukio@gcoop.coop>
2013-12-19 18:50:53 -0300

    Movemos la parte de facturacion que teniamos en cooperative_ar a account_invoice_ar

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 from trytond.pool import Pool
 
-from .invoice import *
+from .address import *
 from .company import *
+from .invoice import *
+from .party import *
 from .pos import *
 
 def register():
     Pool.register(
+        AfipWSTransaction,
+        Address,
+        Company,
+        Invoice,
+        Party,
         Pos,
         PosSequence,
-        Invoice,
-        Company,
-        AfipWSTransaction,
         module='account_invoice_ar', type_='model')
