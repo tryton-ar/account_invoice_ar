91d02f9743eedc3a6c6067685db4506994fbcd4d
Luciano Rossi <lukio@gcoop.coop>
2015-10-29 16:20:23 -0300

    account_invoice_ar depende de PySimpleSOAP 1.08.8

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ requires.append('trytonspain_company_logo >= %s.%s, < %s.%s' %
 requires.append('vatnumber>=1.2')
 requires.append('M2Crypto>=0.22.3')
 requires.append('Pillow>=2.8.1')
-requires.append('PySimpleSOAP==1.16')
+requires.append('PySimpleSOAP==1.08.8')
 requires.append('httplib2>=0.9.1')
 requires.append('suds>=0.4')
 
