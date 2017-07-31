a35ec3b58d4a1070171780684f1b3ee931cbc63c
Adrián Bernardi <adrian.bernardi@gmail.com>
2017-07-31 18:41:28 -0300

    Fixes + flake8
    Conflicts:
    	invoice.py
    	pos.py

--- pos.py
+++ pos.py
@@ -1,8 +1,10 @@
-#! -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
+# This file is part of the account_invoice_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Eval
-from trytond.pool import Pool
 
 __all__ = ['Pos', 'PosSequence']
 
@@ -12,26 +14,25 @@ class Pos(ModelSQL, ModelView):
     __name__ = 'account.pos'
 
     number = fields.Integer('Punto de Venta AFIP', required=True,
-        help=u"Prefijo de emisión habilitado en AFIP")
+        help=u'Prefijo de emisión habilitado en AFIP')
     pos_sequences = fields.One2Many('account.pos.sequence', 'pos',
         'Point of Sale')
     pos_type = fields.Selection([
-            ('manual', u'Manual'),
-            ('electronic', u'Electronic'),
-            ('fiscal_printer', u'Fiscal Printer'),
-            ], 'Pos Type', required=True)
+        ('manual', u'Manual'),
+        ('electronic', u'Electronic'),
+        ('fiscal_printer', u'Fiscal Printer'),
+        ], 'Pos Type', required=True)
     pyafipws_electronic_invoice_service = fields.Selection([
-            ('', ''),
-            ('wsfe', u'Mercado interno -sin detalle- RG2485 (WSFEv1)'),
-#            ('wsmtxca',u'Mercado interno -con detalle- RG2904 (WSMTXCA)'),
-            ('wsbfe', u'Bono Fiscal -con detalle- RG2557 (WSMTXCA)'),
-            ('wsfex', u'Exportación -con detalle- RG2758 (WSFEXv1)'),
-        ], u'AFIP Web Service',
-        states={
+        ('', ''),
+        ('wsfe', u'Mercado interno -sin detalle- RG2485 (WSFEv1)'),
+        #('wsmtxca', u'Mercado interno -con detalle- RG2904 (WSMTXCA)'),
+        ('wsbfe', u'Bono Fiscal -con detalle- RG2557 (WSMTXCA)'),
+        ('wsfex', u'Exportación -con detalle- RG2758 (WSFEXv1)'),
+        ], u'AFIP Web Service', depends=['pos_type'], states={
             'invisible': Eval('pos_type') != 'electronic',
             'required': Eval('pos_type') == 'electronic',
-            }, depends=['pos_type'],
-        help=u"Habilita la facturación electrónica por webservices AFIP")
+            },
+        help=u'Habilita la facturación electrónica por webservices AFIP')
 
     @staticmethod
     def default_pos_type():
@@ -52,30 +53,31 @@ class PosSequence(ModelSQL, ModelView):
 
     pos = fields.Many2One('account.pos', 'Point of Sale')
     invoice_type = fields.Selection([
-            ('', ''),
-            ('1', u'01-Factura A'),
-            ('2', u'02-Nota de Débito A'),
-            ('3', u'03-Nota de Crédito A'),
-            ('4', u'04-Recibos A'),
-            ('5', u'05-Nota de Venta al Contado A'),
-            ('6', u'06-Factura B'),
-            ('7', u'07-Nota de Débito B'),
-            ('8', u'08-Nota de Crédito B'),
-            ('9', u'09-Recibos B'),
-            ('10', u'10-Notas de Venta al Contado B'),
-            ('11', u'11-Factura C'),
-            ('12', u'12-Nota de Débito C'),
-            ('13', u'13-Nota de Crédito C'),
-            ('15', u'15-Recibo C'),
-            ('19', u'19-Factura E'),
-            ('20', u'20-Nota de Débito E'),
-            ('21', u'21-Nota de Crédito E'),
-            ], 'Tipo Comprobante AFIP', required=True,
-        help="Tipo de Comprobante AFIP")
+        ('', ''),
+        ('1', u'01-Factura A'),
+        ('2', u'02-Nota de Débito A'),
+        ('3', u'03-Nota de Crédito A'),
+        ('4', u'04-Recibos A'),
+        ('5', u'05-Nota de Venta al Contado A'),
+        ('6', u'06-Factura B'),
+        ('7', u'07-Nota de Débito B'),
+        ('8', u'08-Nota de Crédito B'),
+        ('9', u'09-Recibos B'),
+        ('10', u'10-Notas de Venta al Contado B'),
+        ('11', u'11-Factura C'),
+        ('12', u'12-Nota de Débito C'),
+        ('13', u'13-Nota de Crédito C'),
+        ('15', u'15-Recibo C'),
+        ('19', u'19-Factura E'),
+        ('20', u'20-Nota de Débito E'),
+        ('21', u'21-Nota de Crédito E'),
+        ], 'Tipo Comprobante AFIP', required=True,
+        help='Tipo de Comprobante AFIP')
+    invoice_type_string = invoice_type.translated('invoice_type')
     invoice_sequence = fields.Property(fields.Many2One('ir.sequence',
-            'Sequence', required=True,
-            domain=[('code', '=', 'account.invoice')],
-            context={'code': 'account.invoice'}))
+        'Sequence', required=True,
+        domain=[('code', '=', 'account.invoice')],
+        context={'code': 'account.invoice'}))
 
     def get_rec_name(self, name):
         type2name = {}
