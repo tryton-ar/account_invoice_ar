7dfdb7eaf4b498234a7e9216c97bcbacfd223fec
Adrián Bernardi <adrian.bernardi@gmail.com>
2018-12-26 12:09:54 -0300

    Update tests suite

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,6 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
 try:
     from trytond.modules.account_invoice_ar.tests.tests import suite
 except ImportError:
