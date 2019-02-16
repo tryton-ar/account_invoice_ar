cf519cd35fdf89c89848847e3df0318921d13d45
Luciano Rossi <lukio@gcoop.coop>
2019-02-16 10:46:39 -0300

    fixes #128 do not change pyafipws_concept and billing dates when copy invoice.

--- invoice.py
+++ invoice.py
@@ -344,9 +344,6 @@ class Invoice:
         else:
             default = default.copy()
         default['transactions'] = None
-        default['pyafipws_concept'] = None
-        default['pyafipws_billing_start_date'] = None
-        default['pyafipws_billing_end_date'] = None
         default['pyafipws_cae'] = None
         default['pyafipws_cae_due_date'] = None
         default['pyafipws_barcode'] = None
