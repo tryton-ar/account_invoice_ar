5e1d405a39e4cf8fe8ed6c1a0a2a9831ca488cb3
Luciano Rossi <lukio@gcoop.coop>
2019-06-27 16:12:54 -0300

    fixes #150 raiseamos un error si al conectarse al WSAA falla.
    De esa manera, no propagamos un posible error al comunicarse al
    resto de los webservices (wsfe, wsfex).

--- company.py
+++ company.py
@@ -2,9 +2,12 @@
 # This file is part of the account_invoice_ar module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+import logging
 
 from trytond.model import fields
 from trytond.pool import PoolMeta
+
+logger = logging.getLogger(__name__)
 
 __all__ = ['Company']
 
@@ -74,4 +77,9 @@ class Company(metaclass=PoolMeta):
         auth = afip_auth.authenticate(service, certificate, private_key,
             force=force, cache=cache, wsdl=WSAA_URL)
         auth_data.update(auth)
+        if not auth_data.get('token'):
+            logger.error('Error webservice WSAA: %s', auth_data.get('err_msg'))
+            self.raise_user_error('wrong_pyafipws_mode', {
+                'message': auth_data.get('err_msg'),
+                })
         return auth_data
