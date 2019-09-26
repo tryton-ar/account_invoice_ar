8ffea3604c0bf9d5b27dae100eed03925146980d
Luciano Rossi <lukio@gcoop.coop>
2019-09-26 15:26:51 -0300

    fix #132 Agrega party_iva_condition.
    Inmutar en la invoice el valor de la condición ante iva del cliente.
    El campo party_tax_identifier cumple la misma función.
    Se agrega campo party_iva_condition. Se obtiene del valor que
    tiene cargada la party en el on_change_party. Si no se ha podido
    cargar en ese momento, se setea en el método set_number.
    
    TODO:
    - modificar los reportes y basarnos en esos campos.
    - agregar campo al invoice_form?

--- invoice.py
+++ invoice.py
@@ -336,6 +336,16 @@ class Invoice:
             ('company', '=', Eval('company', -1)),
             ],
         depends=_DEPENDS + ['company', 'type'])
+    party_iva_condition = fields.Selection([
+        ('', ''),
+        ('responsable_inscripto', 'Responsable Inscripto'),
+        ('exento', 'Exento'),
+        ('consumidor_final', 'Consumidor Final'),
+        ('monotributo', 'Monotributo'),
+        ('no_alcanzado', 'No alcanzado'),
+        ], 'Condicion ante IVA', states=_STATES, depends=_DEPENDS)
+    party_iva_condition_string = party_iva_condition.translated(
+        'party_iva_condition')
 
     @classmethod
     def __setup__(cls):
@@ -426,6 +436,16 @@ class Invoice:
     @staticmethod
     def default_pyafipws_anulacion():
         return False
+
+    @staticmethod
+    def default_party_iva_condition():
+        return ''
+
+    def on_change_party(self):
+        super(Invoice, self).on_change_party()
+
+        if self.party and self.party.iva_condition:
+            self.party_iva_condition = self.party.iva_condition
 
     @fields.depends('pos')
     def on_change_with_pos_pos_daily_report(self, name=None):
@@ -755,6 +775,20 @@ class Invoice:
         credit.reference = '%s' % self.number
         credit.pyafipws_cmp_asoc = [self.id]
         return credit
+
+    @classmethod
+    def set_number(cls, invoices):
+        super(Invoice, cls).set_number(invoices)
+
+        for invoice in invoices:
+            # Posted and paid invoices are tested by check_modify so we can
+            # not modify tax_identifier nor number
+            if invoice.state in {'posted', 'paid'}:
+                continue
+            # Generated invoice may not fill the party iva_condition
+            if not invoice.party_iva_condition and invoice.type == 'out':
+                invoice.party_iva_condition = invoice.party.iva_condition
+        cls.save(invoices)
 
     def get_next_number(self, pattern=None):
         pool = Pool()
