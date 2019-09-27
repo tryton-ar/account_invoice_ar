483b2660a307e2295e9bbcbc48d01277c96a24b6
Luciano Rossi <lukio@gcoop.coop>
2019-09-27 14:15:16 -0300

    Remove metaclass python3 way.

--- invoice.py
+++ invoice.py
@@ -1421,8 +1421,10 @@ class InvoiceReport:
         output.close()
         return image
 
-class CreditInvoiceStart(metaclass=PoolMeta):
+class CreditInvoiceStart:
     __name__ = 'account.invoice.credit.start'
+    __metaclass__ = PoolMeta
+
     from_fce = fields.Boolean('From FCE', readonly=True)
     pyafipws_anulacion = fields.Boolean('Anulación',
         states={
@@ -1440,8 +1442,9 @@ class CreditInvoiceStart(metaclass=PoolMeta):
             ]
 
 
-class CreditInvoice(metaclass=PoolMeta):
+class CreditInvoice:
     __name__ = 'account.invoice.credit'
+    __metaclass__ = PoolMeta
 
     def default_start(self, fields):
         Invoice = Pool().get('account.invoice')
