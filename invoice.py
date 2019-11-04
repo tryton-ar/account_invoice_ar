333d1496b83cf7608bc01ccfeaff9ae41eb70b15
Luciano Rossi <lukio@gcoop.coop>
2019-11-04 13:46:21 -0300

    fix #173 verify if service == 'wsfe' to use Reprocesar
    The only ws which imlement it is wsfe.
    wsfex do not implement ws.Reprocesar

--- invoice.py
+++ invoice.py
@@ -1256,7 +1256,7 @@ class Invoice(metaclass=PoolMeta):
                     self.currency, date=self.invoice_date)
             if payments:
                 last_payment = max(payments, key=lambda x: x[0])[0]
-            elif ws.Reprocesar:
+            elif service == 'wsfe' and ws.Reprocesar:
                 last_payment = self.invoice_date
             else:
                 last_payment = Date.today()
