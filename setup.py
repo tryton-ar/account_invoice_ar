879b8b2f251f792f3982021cf68b943c42b2a428
Luciano Rossi <lukio@gcoop.coop>
2020-09-17 16:15:45 -0300

    Add trydevpi.tryton.org as dependency links for dev versions

--- setup.py
+++ setup.py
@@ -72,6 +72,8 @@ requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus'), 'pytz']
 dependency_links = list(LINKS.values())
+if minor_version % 2:
+    dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
