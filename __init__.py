d6ef8883c7124e4c8021cc8cd2d166404caa9573
Luciano Rossi <lukio@gcoop.coop>
2026-07-08 04:54:30 -0300

    Use to register in the Pool

--- __init__.py
+++ __init__.py
@@ -1,37 +1,3 @@
 # This file is part of the account_invoice_ar module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-from . import invoice
-from . import pos
-from . import bank
-from . import party
-from . import currency
-
-__all__ = ['register']
-
-
-def register():
-    Pool.register(
-        pos.Pos,
-        pos.PosSequence,
-        invoice.Invoice,
-        invoice.InvoiceLine,
-        invoice.InvoiceExportLicense,
-        invoice.CreditInvoiceStart,
-        invoice.InvoiceCmpAsoc,
-        invoice.AfipWSTransaction,
-        invoice.RecoverInvoiceStart,
-        invoice.RecoverInvoiceData,
-        bank.BankAccount,
-        party.Party,
-        currency.Currency,
-        module='account_invoice_ar', type_='model')
-    Pool.register(
-        invoice.CreditInvoice,
-        invoice.RecoverInvoice,
-        module='account_invoice_ar', type_='wizard')
-    Pool.register(
-        invoice.InvoiceReport,
-        module='account_invoice_ar', type_='report')
