f4c5b54d80d3c38e6d6ca83a6d501344a4bde192
Luciano Rossi <lukio@gcoop.coop>
2026-07-08 18:00:58 -0300

    Migrate PyAfipWsWrapper to company.pyafipws_authenticate

--- tests/tools.py
+++ tests/tools.py
@@ -3,14 +3,13 @@
 # the full copyright notices and license terms.
 import os
 import sys
-from pyafipws.wsaa import WSAA
 from pyafipws.wsfev1 import WSFEv1
 from pyafipws.wsfexv1 import WSFEXv1
-from proteus import Model
 
+from proteus import Model
 from trytond.modules.company.tests.tools import get_company
 from trytond.modules.party_ar.tests.tools import set_afip_certs
-from trytond.modules.party_ar.afip import PyAfipWsWrapper
+
 
 __all__ = ['create_pos', 'get_pos', 'get_invoice_types',
     'get_tax', 'get_tax_group', 'get_wsfev1', 'get_wsfexv1']
@@ -138,12 +137,9 @@ def get_wsfev1(company=None, config=None):
         company = set_afip_certs(company, config)
 
     URL_WSFEv1 = "https://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL"
-    URL_WSAA = "https://wsaahomo.afip.gov.ar/ws/services/LoginCms?wsdl"
     cache = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cache')
-    crt = str(company.pyafipws_certificate)
-    key = str(company.pyafipws_private_key)
 
-    ta = PyAfipWsWrapper().authenticate('wsfe', crt, key, wsdl=URL_WSAA, cache=cache)
+    ta = company.pyafipws_authenticate(service='wsfe', cache=cache)
     wsfev1 = WSFEv1()
     wsfev1.LanzarExcepciones = True
     wsfev1.SetTicketAcceso(ta)
@@ -158,14 +154,10 @@ def get_wsfexv1(company=None, config=None):
         company = get_company()
         company = set_afip_certs(company, config)
 
-    URL_WSAA = "https://wsaahomo.afip.gov.ar/ws/services/LoginCms?wsdl"
     URL_WSFEXv1 = "https://wswhomo.afip.gov.ar/wsfexv1/service.asmx?WSDL"
-
     cache = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cache')
-    crt = str(company.pyafipws_certificate)
-    key = str(company.pyafipws_private_key)
 
-    ta = PyAfipWsWrapper().authenticate('wsfex', crt, key, wsdl=URL_WSAA, cache=cache)
+    ta = company.pyafipws_authenticate(service='wsfex', cache=cache)
     wsfexv1 = WSFEXv1()
     wsfexv1.LanzarExcepciones = True
     wsfexv1.SetTicketAcceso(ta)
